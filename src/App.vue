bd0130368927d0dd551f6672547c07a5255da71b
ankoaaaa <sagiya.kouta@gmail.com>
2025-07-10 22:11:36 +0900

    change url

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ const getRecommendations = async () => {
     }
 
     // POSTメソッドに変更し、リクエストボディにタイトルリストを詰める
-    const response = await axios.post('http://localhost:8080/api/v1/recommendations', {
+    const response = await axios.post('https://manga-recommend-api.onrender.com/api/v1/recommendations', {[
       titles: titles
     });
     responseData.value = response.data;
